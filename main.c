fe0b67b0cf8a674f6d7c32bc86ffa0ae9319820f
Min Zhang <124503663+PhDnemo@users.noreply.github.com>
2023-11-17 00:23:31 +0100

    add main.c and smallchesslib.h
    1. main.c implemented four functions, inluding getBoard, getFirstMoves,hasOpening, hasBoard. Corresponding testcases also implemented in main.c.
    2. smallchesslib.h is the  c chess library: https://codeberg.org/drummyfish/smallchesslib.

---
+++ main.c
@@ -0,0 +1,265 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+#include "smallchesslib.h"
+
+#define MY_BOARD_START_STATE "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+
+typedef uint8_t (*SCL_RandomFunction)(void);
+
+uint8_t myRandomFunction() {
+    // Your custom randomization logic here
+    return rand() % 256;  // Adjust according to your needs
+}
+
+
+typedef struct {
+    uint8_t squareFrom;
+    uint8_t squareTo;
+    char resultProm;
+} SCL_MoveResult;
+
+
+void putCharacter(char c)
+{
+  putchar(c);
+}
+
+char str[4096];
+
+void putCharStr(char c)
+{
+  char *s = str;
+
+  while (*s != 0)
+    s++;
+
+  *s = c;
+  *(s + 1) = 0;
+}
+
+//Function to return the board state at a given half-move
+SCL_Board* getBoard(SCL_Game *chessgame, int halfMoveCount) {
+    // 创建一个动态分配的 SCL_Board 结构体
+    SCL_Board* result = (SCL_Board*)malloc(sizeof(SCL_Board));
+
+    // 检查内存分配是否成功
+    if (result == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // 复制游戏状态到临时变量
+    SCL_Game tempGame;
+    memcpy(&tempGame, chessgame, sizeof(SCL_Game));
+
+    // 撤销走子直到达到指定的半步数
+    while (tempGame.ply > halfMoveCount) {
+        SCL_gameUndoMove(&tempGame);
+    }
+
+    // 将结果复制到动态分配的内存中
+    memcpy(result, &tempGame.board, sizeof(SCL_Board));
+
+    // 返回动态分配的内存地址
+    return result;
+}
+
+// Function to get the chessgame truncated to its first N half-moves
+SCL_Game getFirstMoves(const SCL_Game* chessgame, int moveCount) {
+    SCL_Game tempGame;
+
+    memcpy(&tempGame, chessgame, sizeof(SCL_Game));
+
+    // 撤销走子直到达到指定的半步数
+    while (tempGame.ply > moveCount) {
+        SCL_gameUndoMove(&tempGame);
+    }
+    return tempGame;
+}
+
+// 判断第一个游戏的开局是否包含在第二个游戏的开局中
+
+bool hasOpening(SCL_Game* game1, SCL_Game* game2) {
+    // 创建临时变量以保存两个游戏的状态
+    SCL_Game tempGame2;
+
+    // 复制游戏状态到临时变量
+    //memcpy(&tempGame1, game1, sizeof(SCL_Game));
+    memcpy(&tempGame2, game2, sizeof(SCL_Game));
+
+    // 获取两个游戏的半步数
+    uint16_t halfMoves1 = game1->ply;
+    uint16_t halfMoves2 = game2->ply;
+
+
+    // 如果第一个Chess开局的长度大于第二个开局，不可能包含在其中
+    if (halfMoves1 > halfMoves2) {
+        return false;
+    }
+    // Undo Chess2,直到halfMoves2与halfMoves1相同
+    while (tempGame2.ply > halfMoves1) {
+        SCL_gameUndoMove(&tempGame2);
+    }
+
+    // 比较两个Game的内容是否完全一致
+    return memcpy(&tempGame2, &game1, sizeof(SCL_Game));
+}
+
+// Function to check if the chessgame contains the given board state in its first N half-moves
+bool hasBoard(SCL_Game* chessgame, const SCL_Board targetBoard, int moveCount) {
+    SCL_Board tempBoard;
+    SCL_Board* result;
+
+    // Iterate through the first N half-moves
+    for (int i = 0; i < moveCount; ++i) {
+        // Extract move information from the previous moves
+        result = getBoard(chessgame, i);
+        memcpy(tempBoard, result, sizeof(SCL_Board));
+        // Check if the current board matches the target board
+        if (strcmp(tempBoard, targetBoard) == 0) {
+            return true;  // Found a match
+        }
+    }
+
+    return false;  // Board not found in the first N half-moves
+}
+
+void printRecord(const SCL_Record record) {
+    for (int i = 0; i < SCL_RECORD_MAX_SIZE; ++i) {
+        printf("%02x ", record[i]);
+        if ((i + 1) % 16 == 0) {
+            printf("\n");
+        }
+    }
+}
+
+int main()
+{
+    SCL_Game game;
+    SCL_Board initial_board, result_board;
+    SCL_SquareSet emptySquareSet = SCL_SQUARE_SET_EMPTY;
+    SCL_Board board = SCL_BOARD_START_STATE;
+
+    // initialization of the chess game, including moves
+    SCL_gameInit(&game,board);
+
+    memcpy(initial_board, game.board, sizeof(SCL_Board));
+    printf("Print board from initial game+++++++++++++++: \n");
+    SCL_printBoard(initial_board, putCharacter, emptySquareSet, 255, SCL_PRINT_FORMAT_UTF8, 4, 1, 0);
+
+    // ********************TestCase of getBoard function ********************
+    // Get the initial board state
+    SCL_Board* result = getBoard(&game, 0);
+    memcpy(initial_board, result, sizeof(SCL_Board));
+
+    printf("Print initial_board from Game->getBoard0: \n");
+    SCL_printBoard(initial_board, putCharacter, emptySquareSet, 255, SCL_PRINT_FORMAT_UTF8, 4, 1, 0);
+
+   //1. Nf3 Nf6 2. c4 g6 3. Nc3 Bg7 4. d4 O-O 5. Bf4 d5
+   // test 3 halfMoves
+    SCL_gameMakeMove(&game, 12, 21, '\0');// 1-1. Nf3
+    //printf("Print record after 1st Move: \n");
+    //printRecord(game.record);
+    printf("Printboard after SCL_gameMakeMoves 1st,before getBoard: \n");
+    SCL_printBoard(game.board, putCharacter, emptySquareSet, 255, SCL_PRINT_FORMAT_UTF8,4,1,0);
+    printf("Printboard after SCL_gameMakeMoves,after getBoard: \n");
+    result = getBoard(&game, 1);
+    memcpy(initial_board, result, sizeof(SCL_Board));
+    SCL_printBoard(initial_board, putCharacter, emptySquareSet, 255, SCL_PRINT_FORMAT_UTF8,4,1,0);
+    free(result);
+
+    SCL_gameMakeMove(&game, 62, 45, '\0');// 1-2. Nf6
+  //  printf("Print record after 2nd Move: \n");
+  //  printRecord(game.record);
+    printf("Printboard after SCL_gameMakeMoves 2nd,before getBoard: \n");
+    SCL_printBoard(game.board, putCharacter, emptySquareSet, 255, SCL_PRINT_FORMAT_UTF8,4,1,0);
+    printf("Printboard after SCL_gameMakeMoves,after getBoard: \n");
+    result = getBoard(&game, 2);
+    memcpy(initial_board, result, sizeof(SCL_Board));
+    SCL_printBoard(initial_board, putCharacter, emptySquareSet, 255, SCL_PRINT_FORMAT_UTF8,4,1,0);
+    free(result);
+
+    result = getBoard(&game, 1);
+    memcpy(initial_board, result, sizeof(SCL_Board));
+    printf("Printboard after SCL_gameMakeMoves 2,after getBoard1: \n");
+    SCL_printBoard(initial_board, putCharacter, emptySquareSet, 255, SCL_PRINT_FORMAT_UTF8,4,1,0);
+    free(result);
+
+    SCL_gameMakeMove(&game, 2, 18, '\0');  // 2-1. c4
+    printf("Printboard after SCL_gameMakeMoves 3rd,before getBoard: \n");
+    SCL_printBoard(game.board, putCharacter, emptySquareSet, 255, SCL_PRINT_FORMAT_UTF8,4,1,0);
+    printf("Printboard after SCL_gameMakeMoves,after getBoard: \n");
+    result = getBoard(&game, 3);
+    memcpy(initial_board, result, sizeof(SCL_Board));
+    SCL_printBoard(initial_board, putCharacter, emptySquareSet, 255, SCL_PRINT_FORMAT_UTF8,4,1,0);
+    free(result);
+
+    result = getBoard(&game, 2);
+    memcpy(initial_board, result, sizeof(SCL_Board));
+    printf("Printboard after SCL_gameMakeMoves 3,after getBoard2\n: ");
+    SCL_printBoard(initial_board, putCharacter, emptySquareSet, 255, SCL_PRINT_FORMAT_UTF8,4,1,0);
+    free(result);
+
+/*
+    SCL_gameMakeMove(&game, 29, 47, '\0');  // 2-2. g6
+    SCL_gameMakeMove(&game, 22, 37, '\0');  // 5. Nc3
+    SCL_gameMakeMove(&game, 9, 42, '\0');  // 6. Bg7
+    SCL_gameMakeMove(&game, 3, 26, '\0');  // 7. d4
+    SCL_gameMakeMove(&game, 57, 57, '\0');  // 8. O-O
+    SCL_gameMakeMove(&game, 26, 46, '\0');  // 9. Bf4
+    SCL_gameMakeMove(&game, 5, 37, '\0');  // 10. d5
+*/
+
+    // ********************TestCase of getFirstMoves function********************
+
+     SCL_Game truncatedGame = getFirstMoves(&game, 2);
+
+     printf("Printboard after getFirstMoves: \n");
+
+     SCL_printBoard(truncatedGame.board, putCharacter, emptySquareSet, 255, SCL_PRINT_FORMAT_UTF8,4,1,0);
+
+     result = getBoard(&truncatedGame, 2);
+     memcpy(initial_board, result, sizeof(SCL_Board));
+     printf("Printboard after getFirstMoves: 2,after getBoard2\n: ");
+     SCL_printBoard(initial_board, putCharacter, emptySquareSet, 255, SCL_PRINT_FORMAT_UTF8,4,1,0);
+     free(result);
+
+   // ********************TestCase of hasOpenings function********************
+    if (hasOpening(&truncatedGame, &game))
+    {
+        printf("The opening moves match!\n");
+    }
+    else
+    {
+        printf("The opening moves do not match!\n");
+    }
+
+
+    // ********************TestCase of hasBoard function********************
+
+    SCL_Board* result1 = getBoard(&game, 2);
+
+    SCL_Board target_board;
+
+    memcpy(target_board, result1, sizeof(SCL_Board));
+
+    free(result1);
+
+    printf("Print target_board**************: \n");
+
+    SCL_printBoard(target_board, putCharacter, emptySquareSet, 255, SCL_PRINT_FORMAT_UTF8,4,1,0);
+
+    if (hasBoard(&game, target_board, 3))
+    {
+        printf("The target board state is found in the first 2half-moves!\n");
+    }
+    else
+    {
+        printf("The target board state is not found in the first 2 half-moves!\n");
+    }
+
+    return 0;
+}
