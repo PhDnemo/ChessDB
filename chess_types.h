e6804baded6d6e4767ae285a91cebc1d78a8ec44
Min Zhang <124503663+PhDnemo@users.noreply.github.com>
2023-11-30 00:16:13 +0100

    Update chess_types.h
    Add SCL_boardToBinary and SCL_boardFromBinary functions

--- chess_types.h
+++ chess_types.h
@@ -27,4 +27,49 @@ void chessgameToSAN(const chessgame* game, char* sanOutput);
 void parseFENToChessboard(const char* fen, chessboard* board);
 void chessboardToFEN(const chessboard* board, char* fenOutput);
 
+
+typedef struct {
+    char san[SAN_MAX_LENGTH]; // SAN representation of the chess game
+    chessboard currentBoard;   // Current state of the chessboard
+    // Additional fields can be added as required
+} chessgame;
+
+
+// Function declarations
+void parseSANToChessgame(const char* san, chessgame* game);
+void appendCharToSANString(char c, char **sanOutput)
+void chessgameToSAN(const chessgame* game, char* sanOutput);
+void parseFENToChessboard(const char* fen, chessboard* board);
+void chessboardToFEN(const chessboard* board, char* fenOutput);
+
+//////////////
+bool SCL_boardFromBinary(StringInfo buf, SCL_Board *board)
+{
+
+    if (buf->len < SCL_BOARD_STATE_SIZE)
+        return false;
+
+    // Read binary data
+    memcpy(*board, buf->data, SCL_BOARD_STATE_SIZE);
+
+    // update buf
+    buf->cursor += SCL_BOARD_STATE_SIZE;
+
+    return true;
+}
+
+bool
+SCL_boardToBinary(StringInfo buf, const SCL_Board *board)
+{
+
+    if (board == NULL)
+        return false;
+
+    // Writes the SCL Board to the binary stream
+    appendBinaryStringInfo(buf, *board, SCL_BOARD_STATE_SIZE);
+
+    return true;
+}
+#endif // CHESS_TYPES_H
+
 #endif // CHESS_TYPES_H
